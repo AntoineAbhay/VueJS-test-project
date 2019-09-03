6fe9e1d57c7213d06224118ff6139803d73e2674
Antoine Abhay <antoineabhay@gmail.com>
2019-09-03 12:23:38 +0200

    Add router and first component

---
+++ src/components/FirstComponent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>{{ msg }}</h3>
+    <p>This is my first paragraph</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "FirstComponent",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+  color: #42b983;
+}
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <FirstComponent msg="FIRST VIEW HOURA" />
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from "@/components/HelloWorld.vue";
+import FirstComponent from "@/components/FirstComponent.vue";
+
+export default {
+  name: "home",
+  components: {
+    FirstComponent,
+    HelloWorld
+  }
+};
+</script>
